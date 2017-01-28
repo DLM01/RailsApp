e7e8b395451d85a4705f4f212f3f0e49119d1547
David <dlm.droid@gmail.com>
2017-01-28 08:28:38 +0000

    Added username columnm, user profile pages, and username sign-up form.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,26 @@
 # The pages controller contains all of the code for any page inside of the /pages
 class PagesController < ApplicationController
+ 
   # back-end code for pages/index
   def index
   end
+  
  # back-end code for pages/home
   def home
   end
+  
  # back-end code for pages/profile
   def profile
+   # grab the username from the URL as :id
+   if (User.find_by_username(params[:id]))
+   @username = params[:id] 
+  else
+   # redirect to 404 (root for now)
+   redirect_to root_path, :notice=> "User not found!"
   end
+  
+ end
+  
  # back-end code for pages/explore
   def explore
   end
